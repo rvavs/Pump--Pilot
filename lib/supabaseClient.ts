7a07ba4d2dc576188b5618469a9cdf5a748a8bba
rvavs <53159212+rvavs@users.noreply.github.com>
2025-08-09 20:14:50 -0400

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,3 +1,7 @@
 "use client";
+
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-export const supabase = createClientComponentClient();
+
+export const createClient = () => {
+  return createClientComponentClient();
+};
